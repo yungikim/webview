65997559be6374b55802de5645d495b170594970
yungikim <dosa777@gmail.com>
2023-02-06 22:32:37 +0900

    Init

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -12,10 +12,13 @@ class MainScreen extends StatefulWidget {
 
 class _MainScreenState extends State<MainScreen> {
 
+  String _url = 'https://flutter.dev';
+  late WebViewController _webViewController;
+
   @override
-  void initState(){
-      super.initState();
-      if (Platform.isAndroid) WebView.platform = AndroidWebView();
+  void initState() {
+    super.initState();
+    if (Platform.isAndroid) WebView.platform = AndroidWebView();
   }
 
   @override
@@ -23,11 +26,53 @@ class _MainScreenState extends State<MainScreen> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('나만의 웹브라우저'),
+        actions: [
+          IconButton(
+            onPressed: () {
+
+            },
+            icon: const Icon(Icons.add),
+          ),
+       //   IconButton(onPressed: (){}, icon: Icon(Icons.more_vert)),
+         PopupMenuButton<String>(
+           onSelected: (value){
+          //   setState(() {
+               _webViewController.loadUrl(value);
+          //   });
+           },
+           itemBuilder: (context) => [
+             PopupMenuItem(
+              child:Text('구글'),
+               value: 'https://www.google.com',
+             ),
+             PopupMenuItem(
+               child:Text('네이버'),
+               value: 'https://www.naver.com',
+             ),
+             PopupMenuItem(
+               child:Text('카카오'),
+               value: 'https://www.kakao.com',
+             )
+           ],
+         )
+        ],
       ),
-      body: WebView(
-        initialUrl: 'https://flutter.dev',
+      body: WillPopScope(
+        onWillPop: () async {
+          if (await _webViewController.canGoBack()){
+            await _webViewController.goBack();
+            return false;
+          }
+          return true;
+        },
+        child: WebView(
+          initialUrl: _url,
+          javascriptMode: JavascriptMode.unrestricted,
+          onWebViewCreated: (controller){
+            _webViewController = controller;
+          },
+        ),
       ),
     );
   }
 }
-
